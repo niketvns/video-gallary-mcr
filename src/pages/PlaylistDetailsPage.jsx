4ff93533c7d5f7d01b1317e33a6c911619ffc141
NIKET KUMAR MISHRA <niketvns97@gmail.com>
2023-07-31 09:41:57 +0530

    fix: optional-chaining

--- src/pages/PlaylistDetailsPage.jsx
+++ src/pages/PlaylistDetailsPage.jsx
@@ -13,15 +13,15 @@ const PlaylistDetailsPage = () => {
         <div className={'playlistDetails flex gap-14'}>
             <Sidebar/>
             <div className='explore-main flex-1 flex flex-col gap-5'>
-                <h1 className='text-2xl font-semibold'>{playlistDetails.title}</h1>
-                {playlistDetails.desc && <p><b>Description: </b>{playlistDetails.desc}</p>}
+                <h1 className='text-2xl font-semibold'>{playlistDetails?.title}</h1>
+                {playlistDetails?.desc && <p><b>Description: </b>{playlistDetails?.desc}</p>}
                 <div className='all-watch-later flex flex-wrap gap-6'>
                     {
-                        playlistDetails.videos.length ?
-                            playlistDetails.videos.map(video => (
+                        playlistDetails?.videos.length ?
+                            playlistDetails?.videos.map(video => (
                                 <VideoCard key={video._id} video={video}/>
                             )):
-                            <p className='text-black/30 pl-4 py-10 text-xl'>Nothing in the {playlistDetails.title}</p>
+                            <p className='text-black/30 pl-4 py-10 text-xl'>Nothing in the {playlistDetails?.title}</p>
                     }
                 </div>
             </div>
